5480f9bf54789d3a58aa4f988996e0dfe4c83761
soysaucecrakr <fourleafeleven@gmail.com>
2015-11-03 13:17:08 -0500

    Fixed text files

--- blog/saveText.py
+++ blog/saveText.py
@@ -47,7 +47,5 @@ for line in range(0, 50):
 	print (gamename)
 	gamefile = number + '.txt'
 	with open(gamefile, 'w+') as f:
-		f.write(gamename + '\t' + '\t' + '\t' + players + '\t' + '\t' + '\t' 
-		+ ages + '\t' + '\t' + '\t' + time + '\t' + '\t' + '\t' + genrename + '\t'
-		+ '\t' + '\t' + description)
+		f.write(gamename +'#'+ players + '#' + ages + " and up" + '#' + time + '#' + genrename + '#' + description)
 		f.close()
